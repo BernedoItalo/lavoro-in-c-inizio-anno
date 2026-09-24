95f870807ce781db739d95863724dd801159f5ca
BernedoItalo <rodriguezbernedo.italomanuel.studente@itispaleocapa.it>
2026-09-24 12:37:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lavoro_info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int= i;
+
+            Console.WriteLine("MENU");
+
+
+            do {
+                 consele.WriteLine("1) Inserimento di un nuovo record")
+                 consele.WriteLine("2) Visualizzazione dei file")
+                 consele.WriteLine("3) modifica di un record")
+                 consele.WriteLine("4) cancellazione di un recod")
+
+
+
+
+
+
+
+                  while (i=!0)
+
+                }
+
+
+        }
+    }
+}
+
